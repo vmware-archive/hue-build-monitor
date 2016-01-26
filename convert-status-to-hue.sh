3a8721debd4b567e28928c57ae9595a160fec3cd
Pivotal <pivotal@dx207.tor.pivotallabs.com>
2016-01-26 14:57:10 -0500

    adds set -x to task script for debugging

--- convert-status-to-hue.sh
+++ convert-status-to-hue.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -ex
 
 if [ "$(cat pipeline-status/failing-builds.json)" = "[]" ]; then
 	echo "{\"config\": {\"on\": \"true\", \"hue\": 25500, \"sat\": 254, \"bri\": 254, \"alert\": \"none\"}}" > hue-config.json
