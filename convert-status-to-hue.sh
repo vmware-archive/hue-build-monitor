1326d98bec4df84f509c28b0bc4b231f8915d831
Gavin Enns <genns@pivotal.io>
2016-02-08 17:22:47 -0500

    adds explicit output to build monitor task [#113286711]
    Signed-off-by: Brenda Chan <brchan@pivotal.io>

--- convert-status-to-hue.sh
+++ convert-status-to-hue.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if [ "$(cat pipeline-status/failing-builds.json)" = "[]" ]; then
-	echo "{\"config\": {\"on\": \"true\", \"hue\": 25500, \"sat\": 254, \"bri\": 254, \"alert\": \"none\"}}" > hue-config.json
+	echo "{\"config\": {\"on\": \"true\", \"hue\": 25500, \"sat\": 254, \"bri\": 254, \"alert\": \"none\"}}" > hue-config-output/hue-config.json
 else 
-	echo "{\"config\": {\"on\": \"true\", \"hue\": 0, \"sat\": 254, \"bri\": 254, \"alert\": \"lselect\"}}" > hue-config.json
+	echo "{\"config\": {\"on\": \"true\", \"hue\": 0, \"sat\": 254, \"bri\": 254, \"alert\": \"lselect\"}}" > hue-config-output/hue-config.json
 fi
